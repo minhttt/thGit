d238316abaf987a69f1d2162e6fbff34b1bf9b1d
mint0000 <86461781+minhttt@users.noreply.github.com>
2024-09-14 14:39:46 +0700

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ import java.io.Console;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
     }
 }
